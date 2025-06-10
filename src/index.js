b92d8e622b25f103efa907480597a16470b3c5fd
kfkudjoe <kfkudjoe@gmail.com>
2025-06-10 18:43:28 +0000

    Second commit to demonstrate a successful basic express.js web streaming service.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send("Hello World");
-});
+console.log("Variables and modules have been loaded...");
 
-console.log("Server is running...")
-app.listen(port);
+app.get(
+	"/video", async (req, res) => {
+		const videoPath = '/home/l3/Desktop/213 - Lost Bonds.mkv';
+		const stats = await fs.promises.stat(videoPath);
+		
+		res.writeHead(
+			200, {
+				"Content-Length": stats.size,
+				"Content-Type": "video/mkv",
+			}
+		);
+		
+		fs.createReadStream(videoPath).pipe(res);
+	}
+);
+
+console.log("Server successfully constructed...");
+
+app.listen(
+	port, 
+	() => {
+		console.log("Microservice listening on port 3000, point your browser at http://localhost:3000/video");
+	}
+);
